550ba78ef50dc9d4a19f3983289993b3b99f1c2d
alenpariyaroct10 <oct10.alenpariyar@gmail.com>
2025-05-04 14:07:52 +0545

    notification model setup

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+Schema::create('notifications', function (Blueprint $table) {
+    $table->id();
+    $table->string('title');
+    $table->string('creator_type')->nullable();
+    $table->unsignedBigInteger('creator_id')->nullable();
+    $table->string('url')->nullable();
+    $table->enum('visibility', ['all', 'students', 'teachers'])->default('all');
+    $table->string('scope_type')->nullable();
+    $table->unsignedBigInteger('scope_id')->nullable();
+    $table->string('subscope_type')->nullable();
+    $table->unsignedBigInteger('subscope_id')->nullable();
+    $table->timestamps();
+});
 class Notification extends Model
 {
-    //
+    protected $table = 'notifications';
+    protected $fillable = ['title', 'creator_type', 'creator_id', 'url', 'visibility', 'scope_type', 'scope_id', 'subscope_type', 'subscope_id'];
 }
